dcc25800f5d730bbd66427c742d4063f09af80ac
Bozhidar Nemski <bojonemski@gmail.com>
2025-10-05 14:54:07 +0300

    initial commit

---
+++ test/price-tracker-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PriceTrackerAws from '../lib/price-tracker-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/price-tracker-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PriceTrackerAws.PriceTrackerAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
